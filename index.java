68b5653f98a0e2b9028b244f7b9987c45e6d764a
Manoj kumar <manoj166174@gmail.com>
2025-03-26 21:38:49 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution{
+  public static void main(String args[]){
+  Scanner sc = new Scanner(System.in);
+  System.out.println("enter any number");
+  int a = sc.nextInt();
+   System.out.println("enter any number");
+   int b = sc.nextInt();
+   
+   System.out.println("you entered"+a+"and"+b);
+   sc.close();
+  }
+}
